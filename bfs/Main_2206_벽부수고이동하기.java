7eaccc47f32209c7d0c8984b7b720a7a655d75e6
nabongbonge <jaeseugi.1927@gmail.com>
2022-11-02 20:03:59 +0900

    [+] 5th BFS algo

--- bfs/Main_2206_벽부수고이동하기.java
+++ bfs/Main_2206_벽부수고이동하기.java
@@ -7,13 +7,20 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+/**
+ * 벽을 깬 사람은 벽을 깬 맵을 기준으로 방문
+ * 벽을 부수지 않은 사람은 벽을 깨지않은 맵을 기준으로 방문
+ * 위와 같은 로직이 가능한 이유는 BFS에서는 자식 노드를 기준으로 다음 방향으로 가는 방향이 동일 합니다.
+ * 그래서 자식 노드들 중 길을 찾아갈때 자식의 자식의 노드들 중 같은 자식의 자식의 노드가 있다면 두번 돌 필요가 없음을 의미합니다.
+ * 한번이로 깬사람은 벽을 깬 좌표를 기준으로 같은 길을 타게 되므로 큐를 생성해서 길을 찾을 필요가 없다.
+ */
 public class Main_2206_벽부수고이동하기 {
 
-    // 0은 이동 가능
+    // 0은 길이라 이동 가능
     // 1은 벽이라 이동 불가능
     // 1, 1 -> N, M 까지의 최단거리
-    //  이때 시작하는 칸과 끝나는 칸도 포함
-    // 벽 한개 까지는 부스기 가능
+    // 이때 시작하는 칸과 끝나는 칸도 포함
+    // 벽 한개 까지는 부수기 가능
     // 최단거리 출력, 불가능하면 -1 출력
     //  (1, 1)과 (N, M)은 항상 0
 
@@ -30,43 +37,56 @@ public class Main_2206_벽부수고이동하기 {
         // 가로
         int m = Integer.parseInt(st.nextToken());
 
-        // 전체 맵
-        int[][] map = new int[n][m];
-        boolean[][] visit = new boolean[n][m];
+        char[][] map = new char[n][m];
+
         for (int i = 0; i < n; i++) {
             String str = br.readLine();
-            for (int j = 0; j < m; j++) {
-                map[i][j] = Integer.parseInt(str.substring(j, j + 1));
-            }
+            map[i] = str.toCharArray();
+        }
+
+        System.out.println(bfs(n, m, map));
+    }
+
+    static private int bfs(int n, int m, char[][] map) {
+        //  1x1 맵인 경우 1로 종료
+        if (n == 1 && m == 1) {
+            return 1;
         }
 
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{0, 0, 1, 0});
-        visit[0][0] = true;
-        int distance = bfs(n, m, map, queue, visit);
-        System.out.println(distance);
 
-    }
-
-    static private int bfs(int n, int m, int[][] map, Queue<int[]> queue, boolean[][] visit) {
+        boolean[][] no_broken = new boolean[n][m];
+        boolean[][] broken = new boolean[n][m];
+        no_broken[0][0] = true;
+        broken[0][0] = true;
 
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
-            System.out.println("("+cur[0] + "," + cur[1]+")");
             for (int d = 0; d < 4; d++) {
                 int ni = cur[0] + di[d];
                 int nj = cur[1] + dj[d];
                 int cnt = cur[2] + 1;
-                if (ni < 0 || nj < 0 || ni > n - 1 || nj > m - 1 || (visit[ni][nj]&&cur[3] != 0)) continue;
+                boolean destroy = (cur[3] == 1);
+
+                // 마지막에 제일 먼저 도달하면 종료 : BFS에서 제일 처음 도착한 큐가 최단거리
                 if (ni == n - 1 && nj == m - 1) {
                     return cnt;
                 }
-                if (map[ni][nj] == 1 && cur[3] == 0) {
-                    visit[ni][nj] = true;
+
+                if (ni < 0 || nj < 0 || ni > n - 1 || nj > m - 1) continue;
+
+                if (map[ni][nj] == '0') {
+                    if (destroy && !broken[ni][nj]) {
+                        broken[ni][nj] = true;
+                        queue.offer(new int[]{ni, nj, cnt, cur[3]});
+                    } else if (!destroy && !no_broken[ni][nj]) {
+                        no_broken[ni][nj] = true;
+                        queue.offer(new int[]{ni, nj, cnt, cur[3]});
+                    }
+                } else if(map[ni][nj] == '1' && !broken[ni][nj]){
+                    if(destroy) continue;
                     queue.offer(new int[]{ni, nj, cnt, 1});
-                }else if(map[ni][nj] == 0) {
-                    visit[ni][nj] = true;
-                    queue.offer(new int[]{ni, nj, cnt , cur[3]});
                 }
             }
         }
