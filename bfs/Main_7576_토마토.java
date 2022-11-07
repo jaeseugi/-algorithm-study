646b4c630aba6be581b641492393fc0c0b07d4be
nabongbonge <jaeseugi.1927@gmail.com>
2022-11-08 01:21:30 +0900

    refactor: 9th BFS algo - current level check logic, unvisited check logic

--- bfs/Main_7576_토마토.java
+++ bfs/Main_7576_토마토.java
@@ -29,66 +29,61 @@ public class Main_7576_토마토 {
         int n = Integer.parseInt(st.nextToken());
 
         int[][] map = new int[n][m];
-        boolean[][] visit = new boolean[n][m];
 
         Queue<int[]> queue = new LinkedList<>();
+
+        // 모두 익지 못할 경우를 체크 하기 위함
+        int zeroCnt = 0;
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
                 // -1인 경운 방문할 필요가 없다.
-                if(map[i][j] == -1) visit[i][j] = true;
-                // 1인 경우 큐에 담고 방문 처리를 한다.
-                else if(map[i][j] == 1) {
-                    visit[i][j] = true;
-                    queue.offer(new int[]{i, j , 0});
+                if (map[i][j] == 1) {
+                    queue.offer(new int[]{i, j});
+                } else if(map[i][j] == 0) {
+                    // 익어여햘 토마토 수 카운팅
+                    zeroCnt++;
                 }
             }
         }
-        int hoour = bfs(n, m, map, visit, queue);
-        // visit중 방문하지 않는 곳이 있다면, 모든 토마토가 익지 못하는 상황이므로 체크를 해줘야한다.
-        System.out.println(everyVisited(visit) ? hoour : -1);
+        int[] sol = bfs(n, m, zeroCnt, map, queue);
+        System.out.println(sol[1] == 0 ? sol[0] : -1);
     }
 
-    private static int bfs(int n, int m, int[][] map, boolean[][] visit, Queue<int[]> queue) {
-
+    private static int[] bfs(int n, int m, int zeroCnt, int[][] map, Queue<int[]> queue) {
         // 시간
-        int hour = 0;
-        // 현재 큐가 가진 시간, 시간이 바뀐는 지점을 체크 하기 위한 비교
-        int temp = 0, change = 0;
+        int hour = -1;
+        // 현재 탐색 레벨의 큐 사이즈
+        int size = queue.size();
 
         while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int i = cur[0];
-            int j = cur[1];
-            int cnt = temp = cur[2];
 
-            // 큐에서 꺼낸 시간 값이 이전의 시간 값이 다르다는것은 시간이 흐른것을 의미 한다.
-            if(change != temp){
-                change = temp;
-                hour++;
-            }
-            // 사방탐색 기준으로 체크
-            for (int d = 0; d < 4; d++) {
-                int ni = i + di[d];
-                int nj = j + dj[d];
-                if (ni < 0 || nj < 0 || ni > n - 1 || nj > m - 1 || visit[ni][nj]) continue;
-                // 0인 경우, 아직 덜 익은 곳이므로 탐색 진행을 위해 큐에 담고 방문 처리를 한다.
-                if (map[ni][nj] == 0) {
-                    visit[ni][nj] = true;
-                    queue.offer(new int[]{ni, nj, cnt+1});
+            // 큐 사이즈 만큼 루프  ➝ 루프 종료시 새로운 레벨의 큐(다음시간)
+            for (int s = 0; s < size; s++) {
+                int[] cur = queue.poll();
+                int i = cur[0];
+                int j = cur[1];
+
+                // 사방탐색 기준으로 체크
+                for (int d = 0; d < 4; d++) {
+                    int ni = i + di[d];
+                    int nj = j + dj[d];
+                    if (ni < 0 || nj < 0 || ni > n - 1 || nj > m - 1 ) continue;
+                    // 0인 경우, 아직 덜 익은 곳이므로 탐색 진행을 위해 큐에 담고 방문 처리를 한다.
+                    if (map[ni][nj] == 0) {
+                        map[ni][nj] = 1;
+                        // 익어여할 토마토 수 차감
+                        --zeroCnt;
+                        queue.offer(new int[]{ni, nj});
+                    }
                 }
             }
+            hour++;
+            // 다음 레벨의 큐 사이즈를 설정
+            size = queue.size();
         }
-        return hour;
-    }
-    private static boolean everyVisited(boolean[][] visit) {
-        for (boolean[] arr : visit) {
-            for (boolean b : arr) {
-                if (b == false) return false;
-            }
-        }
-        return true;
+        return new int[]{hour, zeroCnt};
     }
 }
